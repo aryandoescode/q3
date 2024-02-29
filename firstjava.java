b10397032b7fbe2e862d049809f5238c20cdc5d8
Aryan singh <aryanhungry8@gmail.com@test.24tojbvrnmjelarvnber0nneid.bx.internal.cloudapp.net>
2024-02-29 09:54:07 +0000

    done

--- firstjava.java
+++ firstjava.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+
+public class firstjava {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        System.out.println("Hello Worlds!");
     }
 }
